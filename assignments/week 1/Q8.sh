146cf639ba4333e2a2bd1b5475c6d28b39839a4b
Patrick Grady <patrickrgrady81@gmail.com>
2022-09-23 14:52:40 -0400

    add assignment Q8

---
+++ assignments/week 1/Q8.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+# Palindrome
+# A palindrome is a word, number, phrase, or other sequence of characters which reads the same backward as forward.
+# madam and racecar are palindromes.
+
+# prompt user for input
+read -p "What do you want to check for palindrome: " pal 
+
+# get length of the string
+len=${#pal}
+
+# initialize a variable for the reversed string
+rev=""
+
+# reverse the string
+# loop bacwards through the string
+for (( i=$len-1; i>=0; i-- ))
+do
+    # add the current character where the loop is to the rev string
+    rev=$rev${pal:i:1}
+done
+
+# check the reveresed string with the original
+# if they are the same
+if [ $pal == $rev ]
+then 
+    echo "Yes"
+else
+    echo "No"
+fi
