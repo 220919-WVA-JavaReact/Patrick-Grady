b1f17f65ea394254cf45bd0021db394565e6474a
Patrick Grady <patrickrgrady81@gmail.com>
2022-09-27 15:12:08 -0400

    more info in countries.sql

--- assignments/week2/SQL/countries.sql
+++ assignments/week2/SQL/countries.sql
@@ -7,7 +7,6 @@ CREATE TABLE countries (
 );
 
 INSERT INTO countries
-("country", region, population, area, gdp)
 VALUES ('USA', 'Northern America', 298444215, 9631420, 37800);
 
 INSERT INTO countries
@@ -18,3 +17,15 @@ VALUES ('United Kingdom', 'Western Europe', 60609153, 244820, 27700);
 
 INSERT INTO countries
 VALUES ('Netherlands', 'Western Europe', 16491461, 41526, 28600);
+
+INSERT INTO countries
+VALUES ('China', 'Asia', 1313973713, 9596960, 5000);
+
+INSERT INTO countries
+VALUES ('Canada', 'Northern America', 33098932, 9984670, 29800);
+
+INSERT INTO countries
+VALUES ('Japan', 'Asia', 127463611, 377835, 28200);
+
+INSERT INTO countries
+VALUES ('India', 'Asia', 1095351995, 3287590, 2900);
