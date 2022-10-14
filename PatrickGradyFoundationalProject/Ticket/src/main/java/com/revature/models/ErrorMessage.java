1561a8666946dda15527015b8b292d0903d9d85b
Patrick Grady <patrickrgrady81@gmail.com>
2022-10-14 09:47:44 -0400

    Add ErrorMessage class

---
+++ PatrickGradyFoundationalProject/Ticket/src/main/java/com/revature/models/ErrorMessage.java
@@ -0,0 +1,27 @@
+package com.revature.models;
+
+public class ErrorMessage {
+    private int status;
+    private String msg;
+
+    public ErrorMessage(int status, String msg) {
+        this.status = status;
+        this.msg = msg;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
